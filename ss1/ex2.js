3bef80d6255e24c68eabab3dac42fed40e5e1322
MinhPhap1508 <minhphapclt.email@gmail.com>
2023-09-15 11:08:56 +0700

    bai tap

---
+++ ss1/ex2.js
@@ -0,0 +1,29 @@
+let courses = [ 
+    { 
+      id: 1, 
+      title: "ReactJS Tutorial", 
+      rating: 4.2, 
+    }, 
+    { 
+      id: 2, 
+      title: "Angular Tutorial", 
+      rating: 2.5, 
+    }, 
+    { 
+      id: 3, 
+      title: "VueJS Tutorial", 
+      rating: 3.8, 
+    }, 
+    { 
+      id: 4, 
+      title: "Java Tutorial", 
+      rating: 4, 
+    }, 
+    { 
+      id: 5, 
+      title: "JavaScript Tutorial", 
+      rating: 3.5, 
+    }, 
+  ];
+  let arr = courses.filter(element => element.rating >4).map(ele => ele.id + "-" + ele.title + "-" + ele.rating);
+  console.log(arr);
